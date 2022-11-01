920599e0d6552c8b9dd04ae449b72140dcb2e508
Hind Chenfour <hind.chenfour123@gmail.com>
2022-11-01 11:04:59 +0000

    Add child classes ReponseQCM and ReponseDirecte

---
+++ GestionExam/models/ReponseDirecte.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionExam.models {
+    class ReponseDirecte : Reponse {
+        private double valeur;
+
+        public ReponseDirecte() : base() {
+
+        }
+
+        public ReponseDirecte(int idQst, String reponse, double valeur) : base(idQst, reponse) {
+            this.valeur = valeur;
+        }
+
+        public double GetValeur() {
+            return valeur;
+        }
+
+        public void SetValeur(double valeur) {
+            this.valeur = valeur;
+        }
+    }
+}
---
+++ GestionExam/models/ReponseQCM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionExam.models {
+    class ReponseQCM : Reponse {
+        private int idQst;
+        private bool veracite;
+
+        public ReponseQCM() : base() {
+
+        }
+
+        public ReponseQCM(int idQst, String reponse, bool veracite) : base(idQst, reponse) {
+            this.idQst = idQst;
+            this.veracite = veracite;
+        }
+
+        public int GetIdQst() {
+            return idQst;
+        }
+
+        public void SetIdQst(int idQst) {
+            this.idQst = idQst;
+        }
+
+        public bool GetVeracite() {
+            return veracite;
+        }
+
+        public void SetVeracite(bool veracite) {
+            this.veracite = veracite;
+        }
+    }
+}
