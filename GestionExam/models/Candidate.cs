72437ba258442c7593df4625bdbe1e81eb913da2
Hind Chenfour <hind.chenfour123@gmail.com>
2022-11-01 10:33:40 +0000

    Add Candidate class

---
+++ GestionExam/models/Candidate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionExam.models {
+    class Candidate {
+        private String cne;
+        private String nom;
+        private String prenom;
+
+        public Candidate() {
+
+        }
+
+        public Candidate(String cne, String nom, String prenom) {
+            this.cne = cne;
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+    }
+}
