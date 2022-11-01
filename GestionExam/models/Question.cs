8816503984af972b09faded2730d196b13479c72
Hind Chenfour <hind.chenfour123@gmail.com>
2022-11-01 11:21:37 +0000

    Add Question class

---
+++ GestionExam/models/Question.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionExam.models {
+    class Question {
+        private int id;
+        private String question;
+        private double bareme;
+
+        public Question() {
+
+        }
+
+        public Question(int id, String question, double bareme) {
+            this.id = id;
+            this.question = question;
+            this.bareme = bareme;
+        }
+
+        public void SetId(int id) {
+            this.id = id;
+        }
+
+        public int GetId() {
+            return id;
+        }
+
+        public void SetQuestion(String question) {
+            this.question = question;
+        }
+
+        public String GetQuestion() {
+            return question;
+        }
+
+        public void SetBareme(double bareme) {
+            this.bareme = bareme;
+        }
+
+        public double GetBareme() {
+            return bareme;
+        }
+
+        public int Repondre(Reponse rp) {
+            return rp.GetId();
+        }
+    }
+}
