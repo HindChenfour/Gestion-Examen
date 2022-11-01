f9f477b275dc23eac7dbf5606d1f3820c333031d
Hind Chenfour <hind.chenfour123@gmail.com>
2022-11-01 10:43:18 +0000

    Add Reponse class

---
+++ GestionExam/models/Reponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionExam.models {
+    class Reponse {
+        private int id;
+        private String reponse;
+
+        public Reponse() {
+
+        }
+
+        public Reponse(int id, String reponse) {
+            this.id = id;
+            this.reponse = reponse;
+        }
+
+        public int GetId() {
+            return id;
+        }
+
+        public void SetId(int id) {
+            this.id = id;
+        }
+
+        public String GetReponse() {
+            return reponse;
+        }
+
+        public void SetReponse(String reponse) {
+            this.reponse = reponse;
+        }
+    }
+}
