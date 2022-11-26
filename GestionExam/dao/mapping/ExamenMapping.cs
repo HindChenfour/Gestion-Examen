de9fb544ca1cd468b46b54e65f9f852914597ff7
Hind Chenfour <hind.chenfour123@gmail.com>
2022-11-26 13:09:28 +0000

    Ctreate mapping class

---
+++ GestionExam/dao/mapping/ExamenMapping.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GestionExam.models;
+
+namespace GestionExam.dao.mapping {
+    class ExamenMapping {
+
+        static public Professeur GetProfesseur(params Object[] row) {
+            return new Professeur(row[0].ToString(), row[1].ToString(), row[2].ToString());
+        }
+
+        static public Object[] GetProfesseurRow(Professeur p) {
+            return new Object[] {
+                p.GetEmailAcademique(),
+                p.GetNom(),
+                p.GetPrenom()
+            };
+        }
+
+        static public Candidat GetCandidat(params Object[] row) {
+            return new Candidat(row[0].ToString(), row[1].ToString(), row[2].ToString());
+        }
+
+        static public Object[] GetCandidatRow(Candidat c) {
+            return new Object[] {
+                c.GetEmailAcademique(),
+                c.GetNom(),
+                c.GetPrenom()
+            };
+        }
+
+
+    }
+}
