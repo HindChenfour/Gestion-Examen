f2c6e1e0435ca2570a0c81e1cc4161ea7116d8a4
Hind Chenfour <hind.chenfour123@gmail.com>
2022-11-29 06:48:15 +0000

    modifications

--- GestionExam/ui/NewQuestion.Designer.cs
+++ GestionExam/ui/NewQuestion.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.label = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel = new System.Windows.Forms.Panel();
-            this.qcmBtn = new System.Windows.Forms.Button();
             this.qstDirecteBtn = new System.Windows.Forms.Button();
+            this.qcmBtn = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
             this.panel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,32 +50,10 @@
             // 
             this.panel2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel2.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.panel2.Location = new System.Drawing.Point(38, 222);
+            this.panel2.Location = new System.Drawing.Point(16, 168);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(594, 4);
             this.panel2.TabIndex = 15;
-            // 
-            // panel
-            // 
-            this.panel.Controls.Add(this.qcmBtn);
-            this.panel.Controls.Add(this.qstDirecteBtn);
-            this.panel.Location = new System.Drawing.Point(22, 71);
-            this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(737, 145);
-            this.panel.TabIndex = 16;
-            // 
-            // qcmBtn
-            // 
-            this.qcmBtn.BackColor = System.Drawing.Color.PowderBlue;
-            this.qcmBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.qcmBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.qcmBtn.Location = new System.Drawing.Point(92, 82);
-            this.qcmBtn.Name = "qcmBtn";
-            this.qcmBtn.Size = new System.Drawing.Size(452, 43);
-            this.qcmBtn.TabIndex = 16;
-            this.qcmBtn.Text = "Question Choix Multiple";
-            this.qcmBtn.UseVisualStyleBackColor = false;
-            this.qcmBtn.Click += new System.EventHandler(this.qcmBtn_Click_1);
             // 
             // qstDirecteBtn
             // 
@@ -90,10 +68,34 @@
             this.qstDirecteBtn.UseVisualStyleBackColor = false;
             this.qstDirecteBtn.Click += new System.EventHandler(this.qstDirecteBtn_Click);
             // 
+            // qcmBtn
+            // 
+            this.qcmBtn.BackColor = System.Drawing.Color.PowderBlue;
+            this.qcmBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.qcmBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.qcmBtn.Location = new System.Drawing.Point(92, 82);
+            this.qcmBtn.Name = "qcmBtn";
+            this.qcmBtn.Size = new System.Drawing.Size(452, 43);
+            this.qcmBtn.TabIndex = 16;
+            this.qcmBtn.Text = "Question Choix Multiple";
+            this.qcmBtn.UseVisualStyleBackColor = false;
+            this.qcmBtn.Click += new System.EventHandler(this.qcmBtn_Click);
+            // 
+            // panel
+            // 
+            this.panel.AutoScroll = true;
+            this.panel.Controls.Add(this.qcmBtn);
+            this.panel.Controls.Add(this.qstDirecteBtn);
+            this.panel.Location = new System.Drawing.Point(22, 71);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(737, 145);
+            this.panel.TabIndex = 16;
+            // 
             // NewQuestion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.Azure;
             this.Controls.Add(this.panel);
             this.Controls.Add(this.panel2);
@@ -110,8 +112,8 @@
 
         private System.Windows.Forms.Label label;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel;
-        private System.Windows.Forms.Button qcmBtn;
         private System.Windows.Forms.Button qstDirecteBtn;
+        private System.Windows.Forms.Button qcmBtn;
+        private System.Windows.Forms.Panel panel;
     }
 }
--- GestionExam/ui/NewQuestion.cs
+++ GestionExam/ui/NewQuestion.cs
@@ -14,10 +14,20 @@ namespace GestionExam.ui {
     public partial class NewQuestion : UserControl {
         public static int nbrQ = 0;
         private Question q;
+        private List<NewChoice> choices;
         private RichTextBox questionBlock;
 
         public NewQuestion() {
             InitializeComponent();
+        }
+
+        private void setRichTextBox() {
+            questionBlock = new RichTextBox();
+
+            questionBlock.Name = "qstBlock_RichTxtbox";
+            questionBlock.Location = new Point(25, 10);
+            questionBlock.Font = new Font("Microsoft Sans Serif", 15);
+            questionBlock.Size = new Size(500, 100);
         }
 
         private void qstDirecteBtn_Click(object sender, EventArgs e) {
@@ -28,23 +38,45 @@ namespace GestionExam.ui {
             qcmBtn.Hide();
             qstDirecteBtn.Hide();
 
-            questionBlock = new RichTextBox();
-
-            questionBlock.Name = "qstBlock_RichTxtbox";
-            questionBlock.Location = new Point(25, 10);
-            questionBlock.Font = new Font("Microsoft Sans Serif", 15);
-            questionBlock.Size = new Size(500, 100);
+            setRichTextBox();
 
             panel.Controls.Add(questionBlock);
             panel.Show();
         }
 
         private void qcmBtn_Click(object sender, EventArgs e) {
-            q = new QuestionChoixMultiple();
+            nbrQ++;
+            q = new QuestionChoixMultiple(nbrQ);
+            choices = new List<NewChoice>();
+
+            label.Text = "Question " + q.GetType() + " : ";
+            qcmBtn.Hide();
+            qstDirecteBtn.Hide();
+
+            setRichTextBox();
+
+            NewChoice c = new NewChoice();
+            c.Location = new Point(25, 100);
+
+            Button btnAjouter = new Button();
+
+            btnAjouter.Text = "Ajouter";
+            btnAjouter.Name = "bntAjouter";
+            btnAjouter.Location = new Point(675, 100);
+            btnAjouter.Click += btnAjouterChoix_click;
+            choices.Add(c);
+
+            panel.Controls.Add(questionBlock);
+            panel.Controls.Add(c);
+            panel.Controls.Add(btnAjouter);
+            panel.Show();
         }
 
-        private void qcmBtn_Click_1(object sender, EventArgs e) {
+        private void btnAjouterChoix_click(object sender, EventArgs e) {
+            NewChoice c = new NewChoice();
 
+            choices.Add(c);
+            panel.Controls.Add(c);
         }
 
         public void setQuestionDirecteValue() {
@@ -75,5 +107,6 @@ namespace GestionExam.ui {
         public String getQuestionText() {
             return questionBlock.Text;
         }
+
     }
 }
